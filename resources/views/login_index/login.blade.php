41c158c4b30ba1ca1595a6560b850b2cc1ccc79c
Sofia15M <sofiaortizacevedo2007@gmail.com>
2024-06-22 15:17:26 -0500

    Se crea una plantilla y la interfaz de login con su controlador

---
+++ resources/views/login_index/login.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.plantillaInfo')
+
+@section('title', 'Inicio de sesion')
+
+@push('css')
+    <link rel="stylesheet" href="{{ asset('css/stylelogin.css') }}">
+@endpush
+
+@section('content')
+    <section>
+        <div class="container">
+            <div class="user login">
+                <div class="form-box">
+                    <form method="POST" action="validar.php">
+                        <div class="form-control">
+                            <h1>Bienvenidos</h1>
+                            <br>
+                            <input type="email" name="Gmail_User" required="" placeholder="Ingrese su correo electrónico" autocomplete="off">
+                            <input type="password" name="Contrasena_User" required="" placeholder="Ingrese su Contraseña" autocomplete="off">
+                            <br>
+                            <button type="submit">Iniciar Sesión</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="img-box">
+                    <img src="../IMG/Logo.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
